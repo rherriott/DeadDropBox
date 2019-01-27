a08e7d52e4c2b3e84a2766a3bd3a6034bde06b9c
rherriott <44274807+rherriott@users.noreply.github.com>
2019-01-27 13:59:43 -0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,7 +1,23 @@
+import sys
+import hashlib
 
 
 class InitPacket:
+  #this should store the details about the file to be sent & what to do with it
   def __init__(self):
+    self.commands = ''
+    self.blobsize = 0
+    self.blob
   
-class BataBloc:
-  def__init__(self):
+class DataBlob:
+  def __init__(self):
+    self.data = []
+    self.size = 0
+    self.hash = 0
+  def __init__(self,data):
+    self.data = data
+    self.size = sys.getsizeof(data)
+    self.hash = hashlib.md5(data).hexdigest()
+  def update(self,new):
+    self.data += new
+    
