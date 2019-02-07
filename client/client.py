1c84ee4eca32c4aae8b3c8d10550827856879a0b
nkragas <44122403+nkragas@users.noreply.github.com>
2019-02-07 15:01:45 -0800

    client.py summary

--- client/client.py
+++ client/client.py
@@ -1,3 +1,8 @@
 # the core client code
-
-
+# Client recieves arguements
+# Client opens connection with server
+# Client encrypts file
+# Client sends file, arguments
+# Client waits for server response
+# if server reports all clear, destroy original file
+# else, throw execption, release file
