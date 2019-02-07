a134a14361179f01055131cd4c8e43148008eef7
nkragas <44122403+nkragas@users.noreply.github.com>
2019-02-07 15:23:26 -0800

    client.py basics

--- client/client.py
+++ client/client.py
@@ -1,4 +1,3 @@
-# the core client code
 # Client recieves arguements
 # Client opens connection with server
 # Client encrypts file
@@ -6,3 +5,18 @@
 # Client waits for server response
 # if server reports all clear, destroy original file
 # else, throw execption, release file
+
+# the core client 
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 4321        # The port used by the server
+
+log_file = open("log_" + datetime.now().isoformat() + ".log","w") #I think this will make an ISO timestamped logfile
+sys.stdout = log_file #all "print"s go to a logfile
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    data = s.recv(1024)
+
+print "Client Online: ", datetime.now.isoformat()
